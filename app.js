9a852ce93852dbcb213a9924bcd3ec7aadc7fd20
Satinder Singh Saini <swv095@gmail.com>
2021-09-04 08:50:09 +0530

    Top K frequent elements in array

--- app.js
+++ app.js
@@ -1,26 +1,17 @@
+let arr=[3,2,3,1,2,4,5,5,6,7,7,8,2,3,1,1,1,10,11,5,6,2,4,7,8,5,6]
+let k=10;
 
- function TreeNode(val) {
-     this.val = val;
-     this.left = this.right = null;
- }
-
-var buildTree = function(preorder, inorder) {
-    let preorder_indx = 0;
-    let arrToTree = function(left,right){
-        if(left> right)
-            return null;
-        let root_val = preorder[preorder_indx];
-        let root = new TreeNode(root_val);
-        preorder_indx++;
-        root.left= arrToTree(left,inorder_indx_val_map[root_val]-1);
-
-        root.right=arrToTree(inorder_indx_val_map[root_val]+1, right);
-        return root;
-    }
-    let inorder_indx_val_map = {};
-    inorder.forEach((value,indx)=>{
-        inorder_indx_val_map[value] = indx;
-    });
-    return arrToTree(0, inorder.length-1); 
-};
-console.log(buildTree([3,9,20,15,7], [9,3,15,20,7]));
+let topKFrequent1 = () =>{
+        let kv={};
+        arr.forEach(e=>{
+            if(kv[e]){
+                kv[e] = kv[e] + 1;
+            }else{
+                kv[e] = 1;
+            }
+        });
+        let arrKv = Object.entries(kv);
+        let l = arrKv.length;
+        return Object.entries(kv).sort((a,b)=>a[1]-b[1]).map(e=>Number(e[0])).slice(l-k,l);
+}
+topKFrequent1();
