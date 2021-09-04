6326e7212b07c38c4b0993dd360c0130011d57a8
Satinder Singh Saini <swv095@gmail.com>
2021-09-04 09:12:37 +0530

    Clone Graph

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
-let arr=[3,2,3,1,2,4,5,5,6,7,7,8,2,3,1,1,1,10,11,5,6,2,4,7,8,5,6]
-let k=10;
-
-let topKFrequent1 = () =>{
-        let kv={};
-        arr.forEach(e=>{
-            if(kv[e]){
-                kv[e] = kv[e] + 1;
-            }else{
-                kv[e] = 1;
-            }
+// Definition for a Node.
+ function Node(val, neighbors) {
+     this.val = val === undefined ? 0 : val;
+     this.neighbors = neighbors === undefined ? [] : neighbors;
+ };
+ var cloneGraph = function(node, kv = {}) {
+    if(node){
+        let cpNode = new Node(node.val);
+        kv[cpNode.val] = cpNode;
+        cpNode.neighbors = node.neighbors.map(n=> {
+            if(kv[n.val])
+                return kv[n.val] 
+            else
+                return cloneGraph(n, kv)
         });
-        let arrKv = Object.entries(kv);
-        let l = arrKv.length;
-        return Object.entries(kv).sort((a,b)=>a[1]-b[1]).map(e=>Number(e[0])).slice(l-k,l);
-}
-topKFrequent1();
+        return cpNode;
+    }else
+        return null;    
+};
+ let node1,node2,node3,node4;
+ node1 = new Node(1);
+ node2 = new Node(2);
+ node3 = new Node(3);
+ node4 = new Node(4);
+ node1.neighbors = [node2, node4]
+ node2.neighbors = [node1, node3]
+ node3.neighbors = [node4, node2]
+ node4.neighbors = [node1, node3]
+console.log(cloneGraph(node1));
