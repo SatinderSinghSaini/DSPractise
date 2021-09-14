9ebc2aa9f0e2f17b582e9ed5d1dfbf02aab24623
Satinder Singh Saini <swv095@gmail.com>
2021-09-14 08:20:21 +0530

    Implemented Trie

--- app.js
+++ app.js
@@ -1,38 +1,47 @@
-//[-10,9,20,null,null,15,7];
-//[2,-1]
-//[9,6,-3,null,null,-6,2,null,null,2,null,-6,-6,-6]
-function TreeNode(val, left, right) {
-        this.val = (val===undefined ? 0 : val);
-        this.left = (left===undefined ? null : left);
-        this.right = (right===undefined ? null : right);
+//Trie
+//Insert and search
+let root;
+let ALPHABET_SIZE = 26;
+class TrieNode {
+    constructor(){
+        this.isEndOfWord = false;
+        this.children = [];
+        for(let i=0; i<ALPHABET_SIZE; i++)
+            this.children[i] = null;
     }
-let node1 = new TreeNode(-10);
-let node2 = new TreeNode(9);
-let node3 = new TreeNode(20);
-let node4 = new TreeNode(15);
-let node5 = new TreeNode(7);
-node1.left = node2;
-node1.right = node3;
-node3.left = node4;
-node3.right = node5;
-
-let maxSum;
-var maxPathSum = function(root) {
-    maxSum = 0;
-    if(root.val!==null && root.val!==undefined)
-        calcMaxPathSum(root);
-    return maxSum;
+}
+let search = (word) =>{
+    let level, currentNode = root;
+    for(level=0; level<word.length; level++){
+        let index = word[level].charCodeAt(0) - 'a'.charCodeAt(0);
+        if(!currentNode.children[index])
+            return false;
+        currentNode = currentNode.children[index];
+    }
+    return currentNode.isEndOfWord;
+};
+let insert = (word) =>{
+    let level;
+    let currentNode = root;
+    for(level =0;level < word.length; level++){
+        let index = word[level].charCodeAt(0) - 'a'.charCodeAt(0);
+        if(!currentNode.children[index])
+            currentNode.children[index] = new TrieNode();            
+        currentNode = currentNode.children[index];
+    }
+    currentNode.isEndOfWord = true;
 };
 
-let calcMaxPathSum = (root) =>{
-    let left,right;
-    left = root.left ? calcMaxPathSum(root.left) : 0;
-    right = root.right ? calcMaxPathSum(root.right) : 0;
-    
-    if(!maxSum)
-        maxSum = root.val;
-    else
-        maxSum = Math.max(maxSum, root.val, root.val + left, root.val + right, root.val + left + right);
-    return root.val + Math.max(left, right, 0);
-}
-console.log(maxPathSum(node1))
+root = new TrieNode();
+// Input keys (use only 'a' through 'z' and lower case)
+let keys = ["the", "a", "there", "answer", "any",
+                 "by", "bye", "their"];
+for (i = 0; i < keys.length ; i++)
+    insert(keys[i]);
+
+console.log(root);
+console.log(search('the'));
+console.log(search('their'));
+console.log(search('bi'));
+
+
