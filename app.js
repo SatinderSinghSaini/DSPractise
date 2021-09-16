345a61d36f1dfa328954584de344083563ea88a8
Satinder Singh Saini <swv095@gmail.com>
2021-09-16 08:50:23 +0530

    Design Add and Search Data Structure: Correct Implementation

--- app.js
+++ app.js
@@ -1,73 +1,65 @@
 /**
  * Initialize your data structure here.
  */
- let root
- var TrieNode = function(){
-     this.children = [];
-     this.endOfWord = false;
- }
- var Trie = function() {
-     root = new TrieNode();
- };
- 
- /**
-  * Inserts a word into the trie. 
-  * @param {string} word
-  * @return {void}
-  */
- 
- Trie.prototype.insert = function(word) {
-     let currentNode = root;
-     let level;
-     for(level=0;level<word.length;level++){
-         let index = word[level].charCodeAt(0) - 'a'.charCodeAt(0);
-         if(!currentNode.children[index])
-             currentNode.children[index] = new TrieNode();
-         currentNode = currentNode.children[index];
-     }
-     currentNode.endOfWord = true;
- };
- 
- /**
-  * Returns if the word is in the trie. 
-  * @param {string} word
-  * @return {boolean}
-  */
- Trie.prototype.search = function(word) {
-     let currentNode = root;
-     let level;
-     for(level=0; level<word.length; level++){
-         let index = word[level].charCodeAt(0) - 'a'.charCodeAt(0);
-         if(!currentNode.children[index]) return false;
-         currentNode = currentNode.children[index];
-     }
-     return currentNode.endOfWord;
- };
- 
- /**
-  * Returns if there is any word in the trie that starts with the given prefix. 
-  * @param {string} prefix
-  * @return {boolean}
-  */
- Trie.prototype.startsWith = function(prefix) {
-     let currentNode = root;
-     let level;
-     for(level=0; level<prefix.length; level++){
-         let index = prefix[level].charCodeAt(0) - 'a'.charCodeAt(0);
-         if(!currentNode.children[index]) return false;
-         currentNode = currentNode.children[index];
-     }
-     return true;    
- };
 
 
- let trie = new Trie();
- let keys = ["the", "a", "there", "answer", "any",
-                 "by", "bye", "their"];
+/** 
+ * @param {string} word
+ * @return {void}
+ */
+
+//let ALPHABET_SIZE = 26;
+
+let root;
+
+var TrieNode = function(){
+    this.children = [];
+    this.isEndOfWord = false;
+}
+var WordDictionary = function(){
+        root = new TrieNode();
+}
+
+WordDictionary.prototype.search = function(word){
+    function dfs(level, currentNode){
+        for(let i=level; i<word.length; i++){
+            let index = word[i].charCodeAt(0) - 'a'.charCodeAt(0);
+            if(word[i] === '.') {
+                for(let j=0;j<currentNode.children.length;j++){
+                    if(currentNode.children[j] && dfs(i+1, currentNode.children[j])) return true;                
+                }
+                return false;
+            }
+            else{
+                if(!currentNode.children[index]) return false;
+                currentNode = currentNode.children[index];
+            }            
+        }
+        return currentNode.isEndOfWord;
+    }
+    return dfs(0,root);
+    
+};
+WordDictionary.prototype.addWord = function(word){
+    let level;
+    let currentNode = root;
+    for(level =0;level < word.length; level++){
+        let index = word[level].charCodeAt(0) - 'a'.charCodeAt(0);
+
+        if(!currentNode.children[index])
+            currentNode.children[index] = new TrieNode();            
+        currentNode = currentNode.children[index];
+    }
+    currentNode.isEndOfWord = true;
+};
+
+
+ let trie = new WordDictionary();
+ let keys = ["at", "and", "an", "add"];
 for (i = 0; i < keys.length ; i++)
-    trie.insert(keys[i]);
+    trie.addWord(keys[i]);
 
 console.log(root);
-console.log(trie.search('the'));
-console.log(trie.search('their'));
+console.log(trie.search('a'));
+console.log(trie.search('.at'));
 console.log(trie.search('bi'));
