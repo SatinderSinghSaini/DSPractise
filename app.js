c57dcb96ec47df2dfe8299d0f0d3613c64bdd66f
Satinder Singh Saini <swv095@gmail.com>
2021-10-19 08:39:22 +0530

    Insert Interval

--- app.js
+++ app.js
@@ -1,31 +1,57 @@
-
-function TreeNode(val, left, right) {
-    this.val = (val===undefined ? 0 : val)
-    this.left = (left===undefined ? null : left)
-    this.right = (right===undefined ? null : right)
- }
- let node1 = new TreeNode(1);
- let node2 = new TreeNode(2);
- let node3 = new TreeNode(3);
- let node4 = new TreeNode(4);
- let node5 = new TreeNode(5);
-
- let node6 = new TreeNode(6);
-
- node5.left = node3;
- node5.right = node6;
- node3.left = node2;
- node3.right = node4;
- node2.left = node1;
- let root = node5;
- 
- var lowestCommonAncestor = function(root, p, q) {
-   if(p.val <root.val && q.val < root.val){
-    return lowestCommonAncestor(root.left, p, q);
-   } else if(p.val > root.val && q.val > root.val){
-    return lowestCommonAncestor(root.right, p, q);
-   }
-   return root;
+/**
+ * @param {number[][]} intervals
+ * @param {number[]} newInterval
+ * @return {number[][]}
+ */
+ var insert = function(intervals, newInterval) {
+  let n = intervals.length;
+  let ans = [];
+  if(n === 0)
+      return [newInterval];
+  
+  if(newInterval[1] < intervals[0][0]){
+      ans.push(newInterval);
+      for(let i=0;i<n;i++){
+          ans.push(intervals[i]);
+      }
+      return ans;
+  }
+  if(newInterval[0] > intervals[n-1][1]){        
+      for(let i=0;i<n;i++){
+          ans.push(intervals[i]);
+      }
+      ans.push(newInterval);
+      return ans;
+  }
+  
+  for(let i=0;i<n;i++){
+      let overlap = doesOverlap(intervals[i],newInterval);
+      if(!overlap){
+          ans.push(intervals[i]);
+          if((newInterval[0] > intervals[i][1]) && (newInterval[1] < intervals[i+1][0])){
+              ans.push(newInterval);
+          }
+      }
+      var temp_interval = [];
+      temp_interval[0] = Math.min(intervals[i][0], newInterval[0]);
+      while(i<n && overlap){            
+          temp_interval[1] = Math.max(intervals[i][1], newInterval[1]);
+          if(i === n-1)
+              overlap = false;
+          else
+              overlap = doesOverlap(intervals[i+1], newInterval);
+          i++;
+      }
+      
+      if(temp_interval.length>1){
+          i--;
+          ans.push(temp_interval);   
+      }
+               
+  }
+  return ans;
 };
 
-console.log(lowestCommonAncestor(root, node2, node6));
+function doesOverlap(interval, newInterval){
+  return Math.min(interval[1], newInterval[1]) >= Math.max(interval[0],newInterval[0]);
+}
